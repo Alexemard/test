978909a3c0aa874655cea6021bf86d765d1d9cca
Alex Emard <alexemard@pm.me>
2025-07-30 20:30:39 -0400

    Implement sandwich ordering UI

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders sandwich order heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText(/order a sandwich/i);
+  expect(heading).toBeInTheDocument();
 });
